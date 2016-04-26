65e20cf421b7ec4b036d6042d52cf09511683ccd
Tales Bontempo Cunha <tabocu@gmail.com>
2016-04-26 10:00:12 -0300

    Adiçao de toolbar às Activitys de cadastro

--- app/src/main/java/br/com/blackseed/blackimob/AddInquilinoActivity.java
+++ app/src/main/java/br/com/blackseed/blackimob/AddInquilinoActivity.java
@@ -1,10 +1,11 @@
 package br.com.blackseed.blackimob;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class AddInquilinoActivity extends AppCompatActivity {
 
@@ -12,29 +13,10 @@ public class AddInquilinoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_inquilino);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_add, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_apply) {
-            Toast.makeText(this, "Inquilino criado!", Toast.LENGTH_LONG).show();
-            finish();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
